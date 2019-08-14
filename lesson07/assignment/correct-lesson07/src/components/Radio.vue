2ca5e06c75ee2acada1f3eee66d5c3624b8086a3
georgeme <georgeme@uw.edu>
2019-08-14 07:38:45 -0700

    Adding radio if/else

---
+++ lesson07/assignment/correct-lesson07/src/components/Radio.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+
+  <b-form-group label="How well did you do?">
+    <b-form-radio-group v-model="selected" :options="options" name="radios-success" stacked></b-form-radio-group>
+    <div v-if="selected === 'nailed'">
+      Sweet! But you should rewatch the movies <a href="Movies.vue">HERE</a> to be sure you really, really know them!
+    </div>
+    <div v-else-if="selected === 'meh'">
+      Watch the movies <a href="Movies.vue">HERE</a> to learn what you don't know!
+    </div>
+    <div v-else-if="selected === 'ugh'">
+      Sounds like you'd better watch the movies <a href="Movies.vue">HERE</a> and stat!
+    </div>
+    <div v-else>
+      Choose one
+    </div>
+  </b-form-group>
+
+  <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: 'choose',
+      options: [{
+          text: 'Choose One',
+          value: 'choose'
+        },
+        {
+          text: 'Nailed it!',
+          value: 'nailed'
+        },
+        {
+          text: 'Meh',
+          value: 'meh'
+        },
+        {
+          text: 'Ugh',
+          value: 'ugh'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- lesson07/assignment/correct-lesson07/src/views/Trivia.vue
+++ lesson07/assignment/correct-lesson07/src/views/Trivia.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Accordion />
+    <Radio />
   </div>
 </template>
 
 <script>
 import Accordion from "@/components/Accordion.vue";
+import Radio from "@/components/Radio.vue";
 
 export default {
   name: "trivia",
   components: {
-    Accordion
+    Accordion,
+    Radio
   }
 }
 </script>
