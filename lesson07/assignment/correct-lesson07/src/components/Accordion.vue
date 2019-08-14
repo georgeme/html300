f68ed8878762cb017272f5fd4257bca660aa9160
georgeme <georgeme@uw.edu>
2019-08-13 19:30:45 -0700

    More accordion work

--- lesson07/assignment/correct-lesson07/src/components/Accordion.vue
+++ lesson07/assignment/correct-lesson07/src/components/Accordion.vue
@@ -1,42 +1,34 @@
 <template>
-  <div role="tablist">
-  <b-card no-body class="mb-1">
+<div role="tablist">
+
+  <div class="container d-flex align-items-center justify-content-center">
+    <div class="triviaHeader my-auto">
+      <h3 class="display-3">test your movie knowledge!</h3>
+      <p class="lead">How much do you know about older or lesser known movies? Click each question below when you're ready to see the answer.</p>
+      <img src="@/assets/fun-movie-stuff.png" class="img-fluid rounded-circle" alt="Image of film, 3D glasses, and movie snacks">
+    </div>
+  </div>
+
+  <b-card no-body class="mb-1" v-for="item in accordionItems" :key="item.id">
     <b-card-header header-tag="header" class="p-1" role="tab">
-      <b-button block href="#" v-b-toggle.accordion-1 variant="info">Accordion 1</b-button>
+      <b-button
+      block href="#"
+      v-b-toggle="item.id"
+      variant="primary"> {{ item.button }}
+      </b-button>
     </b-card-header>
-    <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+    <b-collapse id="item.id" accordion="my-accordion" role="tabpanel">
       <b-card-body>
-        <b-card-text>
-          I start opened because
-          <code>visible</code> is
-          <code>true</code>
-        </b-card-text>
-        <b-card-text>{{ text }}</b-card-text>
+        <b-card-text>{{ item.text }}</b-card-text>
       </b-card-body>
     </b-collapse>
   </b-card>
 
-  <b-card no-body class="mb-1">
-    <b-card-header header-tag="header" class="p-1" role="tab">
-      <b-button block href="#" v-b-toggle.accordion-2 variant="info">Accordion 2</b-button>
-    </b-card-header>
-    <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-      <b-card-body>
-        <b-card-text>{{ text }}</b-card-text>
-      </b-card-body>
-    </b-collapse>
-  </b-card>
+  <div class="triviaHeader">
+    <h3 class="display-3">how well did you do?</h3>
+    <p class="lead">If you didn't do very well, you'd better watch the movies <a href="movies.html">HERE</a> and stat!</p>
+  </div>
 
-  <b-card no-body class="mb-1">
-    <b-card-header header-tag="header" class="p-1" role="tab">
-      <b-button block href="#" v-b-toggle.accordion-3 variant="info">Accordion 3</b-button>
-    </b-card-header>
-    <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-      <b-card-body>
-        <b-card-text>{{ text }}</b-card-text>
-      </b-card-body>
-    </b-collapse>
-  </b-card>
 </div>
 </template>
 
@@ -45,17 +37,23 @@ export default {
   name: "Accordion",
   data() {
     return {
-      text: `
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-          richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-          brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-          tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-          assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-          wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
-          vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic
-          synth nesciunt you probably haven't heard of them accusamus labore VHS.
-        `
-    };
+      accordionItems: [
+      {
+        id: 'accordion-1',
+        button: `Who played Burt and Heather Gummer in Tremors?`,
+        text: `Answer: Burt: Michael Gross, Heather: Reba McEntire`
+      }
+    ],
   }
-};
+}
+}
 </script>
+
+<style>
+@media screen and (max-width: 1025px) {
+  h3.display-3 {
+    font-size: 1.5em;
+    font-weight: 600;
+  }
+}
+</style>
